8902890cf478c35ef94c6648b7868513056cac8b
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-06-23 16:05:36 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { Component } from "react";
 
 export class NavBar extends Component {
   render() {
-    return <h1>Hello</h1>;
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>Paraghraph</p>
+      </div>
+    );
   }
 }
